2ef9442887b57a50f51fad3f5b09b904a5f841ea
Alex Selesse <alex@selesse.com>
2012-06-11 14:00:01 -0400

    Added timestamps and logging to synchronize.sh

--- synchronize.sh
+++ synchronize.sh
@@ -1,8 +1,9 @@
-echo "Overwriting $HOME/.vimrc with .vimrc"
-diff .vimrc $HOME/.vimrc
+OUTPUT="$HOME/.backup/config/`date +%Y-%m-%d-%I:%M:%S`.config"
+echo "Overwriting $HOME/.vimrc with .vimrc" >> $OUTPUT
+diff .vimrc $HOME/.vimrc >> $OUTPUT
 cp .vimrc $HOME/.vimrc
-echo "Overwriting $HOME/.vim with folder .vim"
+echo "Overwriting $HOME/.vim with folder .vim" >> $OUTPUT
 cp -rf .vim $HOME/.vim
-echo "Overwriting $HOME/.profile with .profile"
-diff .profile $HOME/.profile
+echo "Overwriting $HOME/.profile with .profile" >> $OUTPUT
+diff .profile $HOME/.profile >> $OUTPUT
 cp .profile $HOME/.profile
