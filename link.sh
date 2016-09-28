d8dffeb8fcb261343d0fed47ff04a17e37ff13bb
Alex Selesse <alex@selesse.com>
2016-09-27 23:28:44 -0400

    Improve logic by making dotfiles/bin files dynamic
    Instead of listing every file to link explicitly, dynamically determine
    it based on the contents of the Git repo.

--- link.sh
+++ link.sh
@@ -9,63 +9,51 @@
 # Assume we're running this script from the dotfiles repository root
 DOTFILES_DIRECTORY=$PWD
 
-cd $HOME
-files=(
-    .bashrc
-    .gitconfig
-    .githelpers
-    .gitignore_global
-    .ideavimrc
-    .inputrc
-    .my.cnf
-    .profile
-    .tmux.conf
-    .vim
-    .vimrc
-    .zshrc
-    bin/find-parent-git
-    bin/get-password
-    bin/git-churn
-    bin/run-command-on-git-revisions
-    bin/who-committed-this
-)
+main() {
+    cd "$DOTFILES_DIRECTORY"
 
-function main {
     # We need $HOME/bin to exist in order to create bin/* symlinks
-    mkdir -p $HOME/bin
-    link_files ${files[@]}
+    mkdir -p "$HOME/bin"
+    shopt -s extglob
+    # Glob all the dotfiles except for .git and .gitignore
+    dotfiles=$(GLOBIGNORE=".git:.gitignore"; echo .??*)
+    bin_files=$(find bin -type f)
+    files=("${dotfiles[@]} ${bin_files[@]}")
 
+    link_files ${files[@]}
     setup_oh_my_zsh
 }
 
-function link_files() {
-    for file in $@ ; do
-        if [ -e $file ] ; then
-            echo $HOME/$file already exists - aborting link
+link_files() {
+    cd "$HOME"
+
+    for file in "$@" ; do
+        if [[ -e $file ]] ; then
+            echo Link for "$HOME/$file" already exists - not doing anything
         else
-            ln -vs $DOTFILES_DIRECTORY/$file $file
+            ln -vs "$DOTFILES_DIRECTORY/$file" "$file"
         fi
     done
 }
 
-function setup_oh_my_zsh {
+setup_oh_my_zsh() {
     install_oh_my_zsh
     setup_oh_my_zsh_theme
 }
 
-function install_oh_my_zsh {
+install_oh_my_zsh() {
     if [ ! -d "$HOME/.oh-my-zsh" ] ; then
         echo "oh-my-zsh isn't installed - cloning from git"
         git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
     fi
 }
 
-function setup_oh_my_zsh_theme {
+setup_oh_my_zsh_theme() {
     CUSTOM_THEME_DIR=$HOME/.oh-my-zsh/custom/themes
 
     if [ ! -L "$CUSTOM_THEME_DIR/aseles.zsh-theme" ] ; then
-        mkdir -p $CUSTOM_THEME_DIR
-        ln -vs $DOTFILES_DIRECTORY/aseles.zsh-theme $CUSTOM_THEME_DIR/aseles.zsh-theme
+        mkdir -p "$CUSTOM_THEME_DIR"
+        ln -vs "$DOTFILES_DIRECTORY/aseles.zsh-theme" "$CUSTOM_THEME_DIR/aseles.zsh-theme"
     fi
 }
 
